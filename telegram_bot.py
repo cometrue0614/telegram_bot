990621804203dd59e7529b1c6a197f8e3cfbb954
Tim Yang <38610821+cometrue0614@users.noreply.github.com>
2021-07-16 10:56:37 +0800

    add telegram bot api

---
+++ telegram_bot.py
@@ -0,0 +1,3 @@
+import telegram
+bot = telegram.Bot(token='1699765380:AAGCt7ccgPsgysPqaLRpWpVNh-OHjM95DSU')
+bot.send_video(chat_id=-467627368, video=open('【python】4小時初學者教學 #python #python教學 #python入門-zdMUJJKFdsU.mp4', 'rb'), supports_streaming=True)
